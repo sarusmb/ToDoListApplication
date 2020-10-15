1dd7d0cecf2324537d251f0a3432b1d28e532f12
sarabahamdun <ssbahamdun@gmail.com>
2020-10-14 18:48:19 -0700

    Task 4 - Added tests for class Task and ToDoList - Implemented Task and ToDoList

--- src/main/ui/ToDoApplication.java
+++ src/main/ui/ToDoApplication.java
@@ -8,9 +8,8 @@ public class ToDoApplication {
     private Scanner scanner;
 
     public ToDoApplication() {
-        scanner = new Scanner(System.in);
-
-
+        scanner = new Scanner(System.in); // source = BO4-Simple-Calculator-Starter-Lecture-lab
+        System.out.println("Please select an option : add task, remove task, view task or quit");
 
 
     }
